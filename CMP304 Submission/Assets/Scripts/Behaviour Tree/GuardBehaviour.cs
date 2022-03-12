2fd4a0cc8d91a43cf6972dce7df070b3a22dca61
RowanBoat <56042901+RowanBoat@users.noreply.github.com>
2022-03-12 17:50:31 +0000

    Minor cleanup and tree layout started in scripts

--- CMP304 Submission/Assets/Scripts/Behaviour Tree/GuardBehaviour.cs
+++ CMP304 Submission/Assets/Scripts/Behaviour Tree/GuardBehaviour.cs
@@ -18,57 +18,26 @@ public class GuardBehaviour : TreeNode
     {
         seeker = GetComponent<Seeker>();
         Node root = new Patrol(transform, waypoints, seeker);
+
+        //Node root = new Selector(new List<Node>
+        //{
+        //    new Selector(new List<Node>
+        //    {
+        //        new Patrol(transform, waypoints, seeker),
+        //        new Attack()
+        //    }),
+
+        //    new Sequence(new List<Node>
+        //    {
+        //        new MoveToAlert(),
+        //        new Selector(new List<Node>
+        //        {
+        //            new Search(),
+        //            new Attack()
+        //        }
+        //    }
+        //});
+
         return root;
     }
-
-    //void UpdatePath()
-    //{
-    //    if (seeker.IsDone())
-    //        seeker.StartPath(transform.position, alertPos, OnPathComplete);
-    //}
-
-    //void OnPathComplete(Path p)
-    //{
-    //    if (!p.error)
-    //    {
-    //        path = p;
-    //        currentWaypoint = 0;
-    //    }
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        time = Time.time;
-    //        alertPos = mousePos;
-    //    }
-
-    //    if (path == null)
-    //        return;
-
-    //    if (currentWaypoint >= path.vectorPath.Count)
-    //    {
-    //        reachedEnd = true;
-    //        return;
-    //    }
-    //    else
-    //    {
-    //        reachedEnd = false;
-    //    }
-
-    //    Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - new Vector2(transform.position.x, transform.position.y)).normalized;
-    //    Vector2 force = direction * speed * Time.deltaTime;
-
-    //    transform.Translate(force);
-
-    //    float distance = Vector2.Distance(transform.position, path.vectorPath[currentWaypoint]);
-
-    //    if (distance < waypointDistance)
-    //    {
-    //        currentWaypoint++;
-    //    }
-    //}
 }
